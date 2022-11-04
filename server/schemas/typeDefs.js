948eae36a6a57eb9111f4db17826827a2c5e3258
Dylan Martin <dylanmartin@MacBook-Air.attlocal.net>
2022-11-04 13:10:24 -0500

    Added JSON Web Tokens on login and when a addUser

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,6 +12,11 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -36,8 +41,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!): User
-    addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
