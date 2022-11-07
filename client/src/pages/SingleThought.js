0da9d6205511564956fbc9b4c76467787667eeb8
Dylan Martin <dylanmartin@MacBook-Air.attlocal.net>
2022-11-07 13:59:41 -0600

    Single Thought page set up with ReactionList component added into SingleThought

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
-  console.log(thoughtId);
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }
+  })
+
+  const { createdAt, reactionCount, reactions, thoughtText, username } = data?.thought || [];
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
+          <Link
+            to={`/profile/${username}`}
+            style={{ fontWeight: 700 }}
+            className="text-light"
+          >
+            {username}
+          </Link>{' '}
+          thought on {createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thoughtText}</p>
         </div>
       </div>
+      {reactionCount ? <ReactionList reactions={reactions} /> : ''}
     </div>
   );
 };
