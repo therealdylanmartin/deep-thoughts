3f6aff98419ac33a12d689bdb4893eeb813e56d3
Dylan Martin <dylanmartin@MacBook-Air.attlocal.net>
2022-11-07 12:04:13 -0600

    Added concurrently npm package to manage server and client through single develop command

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const db = require('./config/connection');
 // authentication middleware
 const { authMiddleware } = require('./utils/auth');
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -18,6 +20,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
